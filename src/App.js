2ce151df3803b09074b7797f8381f7afe898e192
Haardik <ravat.1@iitj.ac.in>
2022-02-14 02:47:49 +0530

    header issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 
 import React from 'react';
 import {CssBaseline, Grid} from '@material-ui/core';
-import header from './components/header/header';
+import header from './components/header/header.jsx';
 import map from './components/map/map';
 import list from './components/list/list';
 
 
 const App=() =>{
   return (
-    <>
+<div>
+
+<>
+
       <CssBaseline />
       <header/>
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
           <list/>
         </Grid>
+       
         <Grid item xs={12} md={8}>
           <map/>
         </Grid>
       </Grid>
     </>
+</div>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles.js';
 
 const header=()=>{
 
-    return(
-        <h1>header</h1>
-    );
+    return (
+       <h1>sdff</h1>
+      );
 }
 
 export default header;
