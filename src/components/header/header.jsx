a96cdb8aefee8ecd9cdff1b10294116ad61a5696
Haardik <ravat.1@iitj.ac.in>
2022-02-14 03:00:32 +0530

    bug fixes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-
-import useStyles from './styles.js';
 
 const header=()=>{
 
