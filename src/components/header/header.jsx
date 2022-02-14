26896f3e626a40a5a83933a271bac9ce61164f27
Haardik <ravat.1@iitj.ac.in>
2022-02-14 16:09:24 +0530

    css changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Autocomplete } from '@react-google-maps/api';
+import {Appbar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons';
 const header=()=>{
 
     return (
-       <h1>sdff</h1>
+<Appbar position='static'>
+<Toolbar className={classes.toolbar}>
+<Typography variant='h5' className={classes.title}>Maps</Typography>
+
+<Box display='flex'>
+
+<Typography variant='h6' className={classes.title}><Search></Search></Typography>
+
+<Autocomplete>
+
+<div className={classes.search}>
+    <div className={classes.icon}>
+<SearchIcon/>
+
+    </div>
+
+<Inputbase placeholder="Search..."  classes={{root: classes.InputRoot, input:classes.inputInput}}/>
+</div>
+
+</Autocomplete>
+
+</Box>
+
+</Toolbar>
+</Appbar>
+
+       
       );
 }
 
