129cd80beb96e5baa300d17b3fcf55b4a05b2cfc
Haardik <ravat.1@iitj.ac.in>
2022-02-14 18:58:07 +0530

    api keys

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const map=()=>{
 
+const Map=()=>{
+const classes=useStyles();
+const isMobile=useMediaQuery('(min-width:600px)');
+
+const coordinates={lat:0, lng:0};
     return(
-        <h1>map</h1>
+        <div className={classes.mapContainer}>
+
+<GoogleMapReact
+bootstrapURLKeys={{key: 'AIzaSyDALRlyRBKfz8uBCIVNeJoJbR_CPy-fncU'}}
+defaultCenter={coordinates}
+center={coordinates}
+defaultZoom={14}
+margin={[50,50,50,50]}
+options={''}
+onClick={''}
+
+onChildClick={''}
+
+>
+
+
+</GoogleMapReact>
+        </div>
     );
 }
 
-export default map;
+export default Map;
