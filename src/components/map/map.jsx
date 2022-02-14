fe8ac9223102375f5c87139df4a60807cffccdb9
Haardik <ravat.1@iitj.ac.in>
2022-02-14 19:13:21 +0530

    iframe tries

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
-
+import useStyles from './styles.js';
 
 const Map=()=>{
 const classes=useStyles();
@@ -12,7 +12,7 @@ const isMobile=useMediaQuery('(min-width:600px)');
 const coordinates={lat:0, lng:0};
     return(
         <div className={classes.mapContainer}>
-
+{/* <iframe src="https://maps.google.com/maps?q=Tangesir%20Dates%20Products&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width=300 height=150 allowfullscreen></iframe> */}
 <GoogleMapReact
 bootstrapURLKeys={{key: 'AIzaSyDALRlyRBKfz8uBCIVNeJoJbR_CPy-fncU'}}
 defaultCenter={coordinates}
